7a719037eedf54796e92be2190cd2143247a3cb0
Pankajkumar0333 <pkumar@franchiseindia.net>
2024-05-11 11:48:55 +0530

    daskdnas

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -81,14 +81,14 @@ class Kernel extends HttpKernel
         //$schedule->call('\App\Http\Controllers\CronController@fixingDangerZonesBrands')->dailyAt('04:00')->timezone('Asia/Kolkata');
 	    //$schedule->call('\App\Http\Controllers\CronController@freeFranchisorLeadInsertion')->dailyAt('02:30')->timezone('Asia/Kolkata');
         //$schedule->call('\App\Http\Controllers\CronController@feedbackMail')->dailyAt('11:30')->timezone('Asia/Kolkata');
-        $schedule->call(\App\Http\Controllers\SitemapController::class . '@sitemap')->dailyAt('05:48')->timezone('Asia/Kolkata');
+        $schedule->call(\App\Http\Controllers\SitemapController::class, 'sitemap')->dailyAt('11:52')->timezone('Asia/Kolkata');
        // $schedule->call('\App\Http\Controllers\CronController@getSscArray')->dailyAt('05:00')->timezone('Asia/Kolkata');
-       $schedule->call(\App\Http\Controllers\CronController::class . '@investorMembershipExpiration')->dailyAt('03:00')->timezone('Asia/Kolkata');
-       $schedule->call(\App\Http\Controllers\CronController::class . '@expireBrands')->dailyAt('02:49')->timezone('Asia/Kolkata');
-       $schedule->call(\App\Http\Controllers\CronController::class . '@sendInvestorPaidData')->dailyAt('10:36')->timezone('Asia/Kolkata');
-       $schedule->call(\App\Http\Controllers\CronController::class . '@weeklyRegistrationReport')->weeklyOn(1,'08:00')->timezone('Asia/Kolkata');
+       $schedule->call(\App\Http\Controllers\CronController::class, 'investorMembershipExpiration')->dailyAt('03:00')->timezone('Asia/Kolkata');
+       $schedule->call(\App\Http\Controllers\CronController::class, 'expireBrands')->dailyAt('02:49')->timezone('Asia/Kolkata');
+       $schedule->call(\App\Http\Controllers\CronController::class, 'sendInvestorPaidData')->dailyAt('10:36')->timezone('Asia/Kolkata');
+       $schedule->call(\App\Http\Controllers\CronController::class, 'weeklyRegistrationReport')->weeklyOn(1,'08:00')->timezone('Asia/Kolkata');
        //$schedule->call('\App\Http\Controllers\CronController@leadVisibilityCron')->dailyAt('10:19')->timezone('Asia/Kolkata');
-        $schedule->call(\App\Http\Controllers\CronController::class . '@insertLeads')->hourly()->between('7:00', '23:00')->timezone('Asia/Kolkata');
+        $schedule->call(\App\Http\Controllers\CronController::class, 'insertLeads')->hourly()->between('7:00', '23:00')->timezone('Asia/Kolkata');
 
     
     
