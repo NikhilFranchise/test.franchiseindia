fcf8d3827c4b66d3848dfcfe98cb3529654ae62e
Pankajkumar0333 <pkumar@franchiseindia.net>
2024-07-11 16:14:06 +0530

    dasd

--- resources/views/includes/brandlanding/layout-1.blade.php
+++ resources/views/includes/brandlanding/layout-1.blade.php
@@ -232,7 +232,7 @@
                         <!-- Insta Apply section start here -->
                         @if (Auth::check() && Auth::user()->profile_type == Config('constants.ProfileType.Investor'))
                             {{-- @dd($expIntVal); --}}
-                            @if ($expIntVal != null || (Auth::user()->membership_type == 1 && $expIntVal->visibility == 0))
+                            @if ($expIntVal != null && (Auth::user()->membership_type == 1 && $expIntVal->visibility == 0 || $expIntVal == null))
                                 @if (!empty($checkData['message']))
                                     {{ $checkData['message'] }}
                                 @endif
