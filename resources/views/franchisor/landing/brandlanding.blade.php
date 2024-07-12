b3535d06a174c88ddc34a0887ecfa51cd1d264d7
Pankajkumar0333 <pkumar@franchiseindia.net>
2024-07-12 14:11:18 +0530

    dasdasdas

--- resources/views/franchisor/landing/brandlanding.blade.php
+++ resources/views/franchisor/landing/brandlanding.blade.php
@@ -544,18 +544,15 @@
                     $('#conactheading').html("Contact Details");
                     $('#ajaxReshideblock').css('display', 'none');
                     $('#ajaxResshowblock').css('display', 'block');
-                    if (data.user){
-                        $('#ceocontact').html(data.user?.ceo_name);
-                    $('#telephonecontact').html(data?.user?.telephone);
-                    $('#addressocontact').html(data?.user>.fran_address + "" + data?.user?.city + "" + data
+                    $('#ceocontact').html(data.user.ceo_name);
+                    $('#telephonecontact').html(data.user.telephone);
+                    $('#addressocontact').html(data.user.fran_address + "" + data.user.city + "" + data
                         .user.state + "" + data.user.pincode);
                     $('#emailcontact').html("<a href='mailto:" + data.user.email +
                         "' target='_blank'>" + data.user.email + "</a>");
                     $('#mobilecontact').html(data.user.mobile);
                     $('#websitecontact').html("<a href='http://" + data.user.website +
                         "' target='_blank'>" + data.user.website + "</a>");
-                    }
-           
                 }
             });
         });
