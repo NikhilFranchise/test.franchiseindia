455ae7398eeec74ddb6d1108f8247d8e9304edf6
Pankajkumar0333 <pkumar@franchiseindia.net>
2024-07-13 12:28:30 +0530

    dasdds

--- resources/views/category/category.blade.php
+++ resources/views/category/category.blade.php
@@ -487,7 +487,102 @@
 
     </div>
 
-
+    <div class="modal fade lg-panel formsection in" id="expandFranchisenew" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">×</span>
+                </button>
+                <div class="frm-sec">
+                    <div id="askMsg" style="display:none;">
+                        <div class="green">
+                            Thank You for Submitting information for Free Advice!
+                        </div>
+                    </div>
+                    <div class="frm-container" id="askForm">
+                        <form id="homepage1" name="homepage1" method="post">
+                            <h2 class="ttl">Free Advice - Ask Our Experts</h2>
+                            <div id="errMsg1" style="display:none;">
+                                <font color="red"> Please select one option..! </font>
+                            </div>
+                            <div class="frm-type">
+                                <div class="radio">
+                                    <label><input type="radio" name="optionsRadios1" id="optionsRadios3"
+                                            checked="" value="franchisor"> Expand My Brand </label>
+                                </div>
+                                <div class="radio">
+                                    <label><input type="radio" name="optionsRadios1" id="optionsRadios1"
+                                            value="investor"> Buy a Franchise</label>
+                                </div>
+                            </div>
+                            <div class="frm-input">
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <div class="usersprite"></div>
+                                    </span>
+                                    <input type="text" class="form-control blur" name="namefreeadvice1"
+                                        id="namefreeadvice1" placeholder="Enter Name" required>
+                                </div>
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <div class="emailsprite"></div>
+                                    </span>
+                                    <input type="text" name="emailfreeadvice" id="emailfreeadvice1"
+                                        class="form-control blur" placeholder="Enter Email" required>
+                                </div>
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <div class="usersprite"></div>
+                                    </span>
+                                    <input type="text" class="form-control blur" maxlength="10"
+                                        name="mobilefreeadvice1" id="mobilefreeadvice1" placeholder="Enter Mobile No"
+                                        required>
+                                </div>
+                                <div class="input-group">
+                                    <span class="input-group-addon"><img
+                                            src="https://www.franchiseindia.com/images/pincode.png"
+                                            alt="pincode"></span>
+                                    <input type="text" name="pincodefreeadvice1" id="pincodefreeadvice1"
+                                        class="form-control blur" maxlength="6" placeholder="Enter Pincode">
+                                </div>
+                                <div class="input-group">
+                                    <span class="input-group-addon height80">
+                                        <div class="addreesssprite"></div>
+                                    </span>
+                                    <textarea class="form-control height80 blur" name="detailsfreeadvice1" id="detailsfreeadvice1"
+                                        placeholder="Enter Details"></textarea>
+                                </div>
+                                <div class="checkbox rm-prop">
+                                    <label>
+                                        <input type="checkbox" name="is_newsletterfreeadvice1"
+                                            id="is_newsletterfreeadvice1" value="1" checked=""> Yes, i want to
+                                        subscribe for weekly Newsletter
+                                    </label>
+                                </div>
+                                <div class="checkbox rm-prop">
+                                    <label>
+                                        <input type="checkbox" name="is_termsagree1" id="is_termsagree1" value="1"
+                                            checked="">
+                                        I agree to the <a href="https://www.franchiseindia.com/terms"
+                                            target="_blank">Terms &amp; Conditions</a>
+                                    </label>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 txt-center" id="sub">
+                                        <input type="submit" id="btnhome1" class="btn btn-default btn-red"
+                                            value="Ask Our Experts">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
     <script type="text/javascript" src="{{ url('awesomplete/awesomplete.js') }}"></script>
 
     <script language="javascript">
@@ -860,100 +955,5 @@
             });
         }
     </script>
-<div class="modal fade lg-panel formsection in" id="expandFranchisenew" tabindex="-1" role="dialog"
-aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog" role="document">
-    <div class="modal-content">
-        <div class="modal-body">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                    aria-hidden="true">×</span>
-            </button>
-            <div class="frm-sec">
-                <div id="askMsg" style="display:none;">
-                    <div class="green">
-                        Thank You for Submitting information for Free Advice!
-                    </div>
-                </div>
-                <div class="frm-container" id="askForm">
-                    <form id="homepage1" name="homepage1" method="post">
-                        <h2 class="ttl">Free Advice - Ask Our Experts</h2>
-                        <div id="errMsg1" style="display:none;">
-                            <font color="red"> Please select one option..! </font>
-                        </div>
-                        <div class="frm-type">
-                            <div class="radio">
-                                <label><input type="radio" name="optionsRadios1" id="optionsRadios3"
-                                        checked="" value="franchisor"> Expand My Brand </label>
-                            </div>
-                            <div class="radio">
-                                <label><input type="radio" name="optionsRadios1" id="optionsRadios1"
-                                        value="investor"> Buy a Franchise</label>
-                            </div>
-                        </div>
-                        <div class="frm-input">
-                            <div class="input-group">
-                                <span class="input-group-addon">
-                                    <div class="usersprite"></div>
-                                </span>
-                                <input type="text" class="form-control blur" name="namefreeadvice1"
-                                    id="namefreeadvice1" placeholder="Enter Name" required>
-                            </div>
-                            <div class="input-group">
-                                <span class="input-group-addon">
-                                    <div class="emailsprite"></div>
-                                </span>
-                                <input type="text" name="emailfreeadvice" id="emailfreeadvice1"
-                                    class="form-control blur" placeholder="Enter Email" required>
-                            </div>
-                            <div class="input-group">
-                                <span class="input-group-addon">
-                                    <div class="usersprite"></div>
-                                </span>
-                                <input type="text" class="form-control blur" maxlength="10"
-                                    name="mobilefreeadvice1" id="mobilefreeadvice1" placeholder="Enter Mobile No"
-                                    required>
-                            </div>
-                            <div class="input-group">
-                                <span class="input-group-addon"><img
-                                        src="https://www.franchiseindia.com/images/pincode.png"
-                                        alt="pincode"></span>
-                                <input type="text" name="pincodefreeadvice1" id="pincodefreeadvice1"
-                                    class="form-control blur" maxlength="6" placeholder="Enter Pincode">
-                            </div>
-                            <div class="input-group">
-                                <span class="input-group-addon height80">
-                                    <div class="addreesssprite"></div>
-                                </span>
-                                <textarea class="form-control height80 blur" name="detailsfreeadvice1" id="detailsfreeadvice1"
-                                    placeholder="Enter Details"></textarea>
-                            </div>
-                            <div class="checkbox rm-prop">
-                                <label>
-                                    <input type="checkbox" name="is_newsletterfreeadvice1"
-                                        id="is_newsletterfreeadvice1" value="1" checked=""> Yes, i want to
-                                    subscribe for weekly Newsletter
-                                </label>
-                            </div>
-                            <div class="checkbox rm-prop">
-                                <label>
-                                    <input type="checkbox" name="is_termsagree1" id="is_termsagree1" value="1"
-                                        checked="">
-                                    I agree to the <a href="https://www.franchiseindia.com/terms"
-                                        target="_blank">Terms &amp; Conditions</a>
-                                </label>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-12 col-md-12 txt-center" id="sub">
-                                    <input type="submit" id="btnhome1" class="btn btn-default btn-red"
-                                        value="Ask Our Experts">
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
+
 @endsection
