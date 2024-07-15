f65a6bb8f018dae71ec162a7896626d6e1d73a62
Pankajkumar0333 <pkumar@franchiseindia.net>
2024-07-15 15:22:11 +0530

    dasdasd

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -42,7 +42,6 @@ use Illuminate\Support\Str;
     asort($states);
 
     if(isset($loc)) {
-        @dd($loc[0]);
         if(!empty($loc[0])) {
             $cities = Config('location.cityArr.'.$loc[0]);
             asort($cities);
