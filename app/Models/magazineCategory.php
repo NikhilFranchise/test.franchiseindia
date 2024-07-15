a774dfd75f58867781da4c0dd18a9243995947f9
Pankajkumar0333 <pkumar@franchiseindia.net>
2024-07-15 12:14:53 +0530

    dasd

--- app/Models/magazineCategory.php
+++ app/Models/magazineCategory.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class magazineCategory extends Model
+class MagazineCategory extends Model
 {
     use HasFactory;
     public $table = 'magazine_categories';
