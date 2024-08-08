2bb194b87d7a82cb6e58d7e597191ac655887050
GaneshFI <pganesh@franchiseindia.net>
2024-08-08 16:18:52 +0530

    js

--- public/newhomepage/assets/js/app.mobile.js
+++ public/newhomepage/assets/js/app.mobile.js
@@ -1,94 +1,139 @@
 $(document).ready(function () {
-    // Sidebar
-    $("#sidebar, #sidebar-login").mCustomScrollbar({
+    $("#sidebar").mCustomScrollbar({
         theme: "minimal"
     });
 
-    // Sidebar toggling
-    $('#dismiss, #dismiss-login, .overlay').on('click', function () {
-        $('#sidebar, #sidebar-login').removeClass('active');
+    $('#dismiss, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
         $('.overlay').removeClass('active');
     });
 
-    $('#sidebarCollapse, #sidebarCollapse-main-login').on('click', function () {
-        $('#sidebar, #sidebar-login').addClass('active');
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').addClass('active');
         $('.overlay').addClass('active');
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
+});
 
-    // Header Shadow on Scroll
-    var header = $('.header');
-    var onScroll = function () {
-        if ($(window).scrollTop() !== 0) {
-            if (!header.hasClass('shadow')) {
-                header.addClass('shadow');
-            }
-        } else {
-            header.removeClass('shadow');
-        }
-    };
-
-    $(window).scroll(onScroll);
-
-    // Swiper Slider
-    var swiper = new Swiper('.swiper-container', {
-        loop: true,
-        slidesPerView: 'auto',
-        spaceBetween: 10,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        scrollbar: {
-            el: '.swiper-scrollbar',
-            hide: true,
-        },
-        keyboard: {
-            enabled: true,
-        },
-        breakpoints: {
-            '@0.00': {
-                slidesPerView: 1.5,
-                spaceBetween: 15,
-            },
-            '@0.25': {
-                slidesPerView: 1.5,
-                spaceBetween: 15,
-            },
-            '@0.50': {
-                slidesPerView: 1.5,
-                spaceBetween: 15,
-            },
-            '@0.75': {
-                slidesPerView: 2.5,
-                spaceBetween: 15,
-            },
-            '@1.00': {
-                slidesPerView: 3.5,
-                spaceBetween: 20,
-            },
-            '@1.50': {
-                slidesPerView: 4,
-                spaceBetween: 25,
-            },
-        }
+//loging section sidebar
+$(document).ready(function () {
+    $("#sidebar-login").mCustomScrollbar({
+        theme: "minimal"
     });
 
-    // Testimonials Carousel
-    $('#testimonial-carousel').owlCarousel({
-        margin: 0,
-        autoplay: true,
-        nav: true,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeInUp',
-        navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-            }
-        }
+    $('#dismiss-login, .overlay').on('click', function () {
+        $('#sidebar-login').removeClass('active');
+        $('.overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse-main-login').on('click', function () {
+        $('#sidebar-login').addClass('active');
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 });
+
+var header = $('.header');
+
+$(window).scroll(function(e){
+    if(header.offset().top !== 0){
+        if(!header.hasClass('shadow')){
+            header.addClass('shadow');
+        }
+    }else{
+        header.removeClass('shadow');
+    }
+});
+//search bar class
+// $(document).scroll(function () {
+// 	myID = document.getElementById("search");
+// 	var a = function () {
+// 		var b = window.scrollY;
+// 		if (b >= 300) {
+// 			myID.className = "search show slide-right"
+// 		} else {
+// 			myID.className = "search hide"
+// 		}
+// 	};
+// 	window.addEventListener("scroll", a)
+// });
+
+var swiper = new Swiper('.swiper-container', {
+    // mousewheel: true,
+    loop: true,
+    slidesPerView: 'auto',
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // autoplay: {
+    //     delay: 1000,
+    //     disableOnInteraction: true,
+    //   },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: true,
+    },
+    keyboard: {
+        enabled: true,
+    },
+    breakpoints: {
+      //samll
+        '@0.00': {
+        slidesPerView:  1.5,
+        spaceBetween: 15,
+      },
+        //xs
+      '@0.25': {
+            slidesPerView:  1.5,
+            spaceBetween: 15,
+        },
+        //tab-mini
+        '@0.50': {
+            slidesPerView:  1.5,
+            spaceBetween: 15,
+        },
+        //ipad
+
+        '@0.75': {
+        slidesPerView: 2.5,
+        spaceBetween: 15,
+      },
+        //desktop-mini
+      '@1.00': {
+        slidesPerView: 3.5,
+        spaceBetween: 20,
+      },
+        //xl-lg desktop
+      '@1.50': {
+        slidesPerView: 4 ,
+        spaceBetween: 25,
+      },
+    }
+  });
+  (function($) {
+    "use strict";
+  
+    /*--/ Testimonials owl /--*/
+    $('#testimonial-carousel').owlCarousel({
+      margin: 0,
+      autoplay: true,
+      nav: true,
+      animateOut: 'fadeOut',
+      animateIn: 'fadeInUp',
+      navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+        }
+      }
+    });
+  
+  })(jQuery);
