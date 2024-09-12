c70624d4cfea1624194063cb9f3b42c3a714ee36
NikhilFranchise <cnikhil@franchiseindia.net>
2024-09-12 15:05:31 +0530

    1209305

--- resources/views/category/navigation-search-by.blade.php
+++ resources/views/category/navigation-search-by.blade.php
@@ -47,7 +47,7 @@ $searchText = request()->query('text');
     $h1 = 'Business opportunities';
    }
    else{
-    $h1 = 'Business opportunities for ' . $searchText ;
+    $h1 = 'Business/Franchise Opportunities Results For '. $searchText . ' - Franchise India' ;
    }
 // dd($searchText);
 }
