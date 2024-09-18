b4afe4eaaca600e589fd279ef9db535e566635a1
GaneshFI <pganesh@franchiseindia.net>
2024-09-18 09:44:18 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -91,7 +91,7 @@ Route::get('https://www.franchiseindia.com/business-opportunities/robotics-and-t
 
 Route::get('content/{slug_and_id}', function ($slug_and_id) {
     // Check if the slug_and_id contains a dot or a dash and split accordingly
-    
+
     if (preg_match('/\.\d+$/', $slug_and_id)) {
         // Replace the dot with a dash only if there is a number after the dot
         $slug_and_id = preg_replace('/\./', '-', $slug_and_id);
@@ -762,6 +762,7 @@ Route::group(['prefix' => 'entrepreneur'], function () {
     });
 });
 Route::get('event', [EventController::class, 'event']);
+Route::get('event-archives',   function() { return view('static.event-archives');});
 
 //Rss Route
 Route::get('rss', [FacebookArticleController::class, 'rss']); // Facebook Instant Articles RSS feed route
