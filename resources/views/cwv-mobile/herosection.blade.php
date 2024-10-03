9b5d9f1c59aba1f310e6e939fd1f42244c158f14
GaneshFI <pganesh@franchiseindia.net>
2024-10-03 12:10:55 +0530

    changes in cwv-mobilr

--- resources/views/cwv-mobile/herosection.blade.php
+++ resources/views/cwv-mobile/herosection.blade.php
@@ -1,3 +1,10 @@
+@php
+    use Illuminate\support\Str;
+    $catArr = Config('constants.CategoryArr');
+    asort($catArr);
+    $states = Config('location.stateArr');
+    asort($states);
+@endphp
 <section class="hero-moblie" id="hero-mobile">
     <div class="lnkblk">
         <a href="https://www.franchiseindia.com/brands/easygym.95394" target="_blank" class="setpat"><img
@@ -26,28 +33,12 @@
                                 class="form-control form-control-custom dropdown-toogle-icon"
                                 id="getMainCategoryDataHeader1" onchange="getSubCategoryHeader1(this.value)">
                                 <option value="" hidden="">Select Industry</option>
-                                <option value="8" slug="automotive">
-                                    Automotive</option>
-                                <option value="1" slug="beauty-and-health">
-                                    Beauty &amp; Health</option>
-                                <option value="6" slug="business-services">
-                                    Business Services</option>
-                                <option value="5" slug="dealers-and-distributors">
-                                    Dealers &amp; Distributors</option>
-                                <option value="3" slug="education">
-                                    Education</option>
-                                <option value="10" slug="fashion">
-                                    Fashion</option>
-                                <option value="2" slug="food-and-beverage">
-                                    Food And Beverage</option>
-                                <option value="7" slug="home-based-business">
-                                    Home Based Business</option>
-                                <option value="263" slug="hotels-and-resorts">
-                                    Hotel, Travel &amp; Tourism</option>
-                                <option value="9" slug="retail">
-                                    Retail</option>
-                                <option value="11" slug="sports-fitness-and-entertainment">
-                                    Sports, Fitness &amp; Entertainment</option>
+                                @foreach ($catArr as $index => $value)
+                                    <option value="{{ $index }}"
+                                        slug="{{ Config('category.SeoCategoryArr.' . $index) }}"
+                                        @if (isset($mc) && $index == $mc) selected @endif>
+                                        {{ $value }}</option>
+                                @endforeach
 
                             </select>
                         </li>
@@ -94,28 +85,12 @@
                                                    dropdown-toogle-icon"
                                 name="mc" id="getMainCategoryDataHeaderLoc1">
                                 <option value="" hidden="">Select Industry</option>
-                                <option value="8" slug="automotive">
-                                    Automotive</option>
-                                <option value="1" slug="beauty-and-health">
-                                    Beauty &amp; Health</option>
-                                <option value="6" slug="business-services">
-                                    Business Services</option>
-                                <option value="5" slug="dealers-and-distributors">
-                                    Dealers &amp; Distributors</option>
-                                <option value="3" slug="education">
-                                    Education</option>
-                                <option value="10" slug="fashion">
-                                    Fashion</option>
-                                <option value="2" slug="food-and-beverage">
-                                    Food And Beverage</option>
-                                <option value="7" slug="home-based-business">
-                                    Home Based Business</option>
-                                <option value="263" slug="hotels-and-resorts">
-                                    Hotel, Travel &amp; Tourism</option>
-                                <option value="9" slug="retail">
-                                    Retail</option>
-                                <option value="11" slug="sports-fitness-and-entertainment">
-                                    Sports, Fitness &amp; Entertainment</option>
+                                @foreach ($catArr as $index => $value)
+                                    <option value="{{ $index }}"
+                                        slug="{{ Config('category.SeoCategoryArr.' . $index) }}"
+                                        @if (isset($mc) && $index == $mc) selected @endif>
+                                        {{ $value }}</option>
+                                @endforeach
 
                             </select>
                         </li>
@@ -126,77 +101,11 @@
                                                    dropdown-toogle-icon"
                                 onchange="getcity1(this.value)">
                                 <option value="" hidden="">Select a State</option>
-                                <option value="35" slug="andaman-and-nicobar">
-                                    Andaman and Nicobar</option>
-                                <option value="1" slug="andhra-pradesh">
-                                    Andhra Pradesh</option>
-                                <option value="2" slug="arunachal-pradesh">
-                                    Arunachal Pradesh</option>
-                                <option value="3" slug="assam">
-                                    Assam</option>
-                                <option value="4" slug="bihar">
-                                    Bihar</option>
-                                <option value="5" slug="chandigarh">
-                                    Chandigarh</option>
-                                <option value="6" slug="chhattisgarh">
-                                    Chhattisgarh</option>
-                                <option value="7" slug="daman-and-diu">
-                                    Daman and Diu</option>
-                                <option value="23" slug="delhi">
-                                    Delhi</option>
-                                <option value="8" slug="goa">
-                                    Goa</option>
-                                <option value="9" slug="gujarat">
-                                    Gujarat</option>
-                                <option value="10" slug="haryana">
-                                    Haryana</option>
-                                <option value="11" slug="himachal-pradesh">
-                                    Himachal Pradesh</option>
-                                <option value="12" slug="jammu-and-kashmir">
-                                    Jammu and Kashmir</option>
-                                <option value="13" slug="jharkhand">
-                                    Jharkhand</option>
-                                <option value="14" slug="karnataka">
-                                    Karnataka</option>
-                                <option value="15" slug="kerala">
-                                    Kerala</option>
-                                <option value="16" slug="lakshadweep">
-                                    Lakshadweep</option>
-                                <option value="17" slug="madhya-pradesh">
-                                    Madhya Pradesh</option>
-                                <option value="18" slug="maharashtra">
-                                    Maharashtra</option>
-                                <option value="19" slug="manipur">
-                                    Manipur</option>
-                                <option value="20" slug="meghalaya">
-                                    Meghalaya</option>
-                                <option value="21" slug="mizoram">
-                                    Mizoram</option>
-                                <option value="22" slug="nagaland">
-                                    Nagaland</option>
-                                <option value="24" slug="odisha">
-                                    Odisha</option>
-                                <option value="25" slug="pondicherry">
-                                    Pondicherry</option>
-                                <option value="26" slug="punjab">
-                                    Punjab</option>
-                                <option value="27" slug="rajasthan">
-                                    Rajasthan</option>
-                                <option value="28" slug="sikkim">
-                                    Sikkim</option>
-                                <option value="29" slug="tamil-nadu">
-                                    Tamil Nadu</option>
-                                <option value="34" slug="telangana">
-                                    Telangana</option>
-                                <option value="30" slug="tripura">
-                                    Tripura</option>
-                                <option value="32" slug="uttar-pradesh">
-                                    Uttar Pradesh</option>
-                                <option value="31" slug="uttarakhand">
-                                    Uttarakhand</option>
-                                <option value="33" slug="west-bengal">
-                                    West Bengal</option>
-
+                                @foreach ($states as $index => $value)
+                                    <option value="{{ $index }}" slug="{{ strtolower(Str::slug($value)) }}"
+                                        @if (isset($loc[0]) && $loc[0] == $index) selected @endif>
+                                        {{ $value }}</option>
+                                @endforeach
                             </select>
                         </li>
                         <li class="p-0 m-0">
@@ -229,29 +138,12 @@
                             <select aria-label="industry" name="mc" id="getMainCategoryDataHeaderInv1"
                                 class="form-control form-control-custom dropdown-toogle-icon">
                                 <option value="" hidden="">Select Industry</option>
-                                <option value="8" slug="automotive">
-                                    Automotive</option>
-                                <option value="1" slug="beauty-and-health">
-                                    Beauty &amp; Health</option>
-                                <option value="6" slug="business-services">
-                                    Business Services</option>
-                                <option value="5" slug="dealers-and-distributors">
-                                    Dealers &amp; Distributors</option>
-                                <option value="3" slug="education">
-                                    Education</option>
-                                <option value="10" slug="fashion">
-                                    Fashion</option>
-                                <option value="2" slug="food-and-beverage">
-                                    Food And Beverage</option>
-                                <option value="7" slug="home-based-business">
-                                    Home Based Business</option>
-                                <option value="263" slug="hotels-and-resorts">
-                                    Hotel, Travel &amp; Tourism</option>
-                                <option value="9" slug="retail">
-                                    Retail</option>
-                                <option value="11" slug="sports-fitness-and-entertainment">
-                                    Sports, Fitness &amp; Entertainment</option>
-
+                                @foreach ($catArr as $index => $value)
+                                    <option value="{{ $index }}"
+                                        slug="{{ Config('category.SeoCategoryArr.' . $index) }}"
+                                        @if (isset($mc) && $index == $mc) selected @endif>
+                                        {{ $value }}</option>
+                                @endforeach
                             </select>
                         </li>
                         <li class="p-0 m-0">
---
+++ resources/views/cwv-mobile/upcoming-events.blade.php
@@ -0,0 +1,43 @@
+<section class="upcoming-events">
+    <h2 class="brands-head">{{ Request::segment(1) == 'hi' ? 'आने वाले कार्यक्रम' : 'Upcoming Events' }}</h2>
+
+    <div class="card-wrap">
+        <div class="leading-card">
+            <div class="brand-ins">
+                <a href="https://www.franchiseindia.net/fro/ahmedabad/" target="_blank">
+                    <img loading="lazy" alt="FROEXPO Gujarat" src="{{ url('cvw/event-videos/fro-gujarat.webp') }}"
+                        width="345" height="208"></a>
+            </div>
+            <h2>FROEXPO Ahmedabad</h2>
+            <p>21-22 September 2024, Mahatma Mandir Convention...</p>
+            <a href="https://www.franchiseindia.net/fro/ahmedabad/" target="_blank" class="know-more">Registration</a>
+            <div class="hotline">Hotline:<span>+91 9311254088</span></div>
+
+        </div>
+
+        <div class="leading-card">
+            <div class="brand-ins">
+                <a href="https://www.restaurantindia.in/congress/" target="_blank">
+                    <img loading="lazy" alt="Indian Restaurant Congress & Awards 2024"
+                    src="{{url('cvw/event-videos/restaurant.webp')}}" width="345" height="208"></a>
+            </div>
+            <h2>Indian Restaurant Congress & Awards 2024</h2>
+            <p>8-9 October 2024, YASHOBHOOMI, (IICC), New Delhi</p>
+            <a href="https://www.restaurantindia.in/congress/" target="_blank" class="know-more">Registration</a>
+            <div class="hotline">Hotline: <span>+91 9717683838</span></div>
+        </div>
+
+
+        <div class="leading-card">
+            <div class="brand-ins">
+                <img src="https://test.franchiseindia.com/dotcom-mobile-fresh-code/images/fro-gujarat.webp"
+                    alt="FRO Gujarat" width="345" height="208" loading="lazy">
+            </div>
+            <h2>Thailand Franchise &amp; Distribution Show</h2>
+            <p>4 October 2024</p>
+            <a href="https://www.franchiseindia.com/brands/seeds-pre-school.85746" class="know-more">Registration</a>
+            <div class="hotline">Hotline: <span>+91 9717683838</span></div>
+        </div>
+
+    </div>
+</section>
