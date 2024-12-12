36e95ac0b40f2fc83b05b296fd99b61d9b980f63
GaneshFI <pganesh@franchiseindia.net>
2024-12-12 16:21:13 +0530

    changes

---
+++ resources/views/insights/sitemaps/english_sitemap.blade.php
@@ -0,0 +1,27 @@
+<sightmapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <sitemap>
+        <loc>{{url('/')}}/insights/en/news.xml</loc>
+    </sitemap>
+    <sitemap>
+        <loc>{{url('/')}}/insights/en/article.xml</loc>
+    </sitemap>
+    <sitemap>
+        <loc>{{url('/')}}/insights/en/interview.xml</loc>
+    </sitemap>
+    <sitemap>
+        <loc>{{url('/')}}/insights/en/report.xml</loc>
+    </sitemap>
+    <sitemap>
+        <loc>{{url('/')}}/insights/en/event.xml</loc>
+    </sitemap>
+    <sitemap>
+        <loc>{{url('/')}}/insights/en/categories.xml</loc>
+    </sitemap>
+    <sitemap>
+        <loc>{{url('/')}}/insights/en/subcategories.xml</loc>
+    </sitemap>
+    <sitemap>
+        <loc>{{url('/')}}/insights/en/tags.xml</loc>
+    </sitemap>
+
+    </sightmapindex>
--- routes/web.php
+++ routes/web.php
@@ -1023,21 +1023,37 @@ Route::group(['prefix' => 'admin'], function () {
 
 Route::get('location/{city}',              [BusinessListingController::class, 'listingLocation']);
 // INSIGHTS ROUTES START HERE //
-Route::get('insights/sitemap.xml', function () {
-    return response()->view('insights.sitemaps.sitemap')->header('Content-type', 'text/xml');
+Route::get('insights/en/sitemap.xml', function () {
+    return response()->view('insights.sitemaps.english_sitemap')->header('Content-type', 'text/xml');
+});
+Route::get('insights/hi/sitemap.xml', function () {
+    return response()->view('insights.sitemaps.hindi_sitemap')->header('Content-type', 'text/xml');
 });
 /*Language setter*/
 
 Route::group(['prefix' => 'insights'], function () {
-    Route::get('news.xml',                      [InsightSitemapController::class, 'newssitemap']);
-    Route::get('article.xml',                   [InsightSitemapController::class, 'articlesitemap'])->name('article.xml');
-    Route::get('interview.xml',                 [InsightSitemapController::class, 'interviewsitemap'])->name('interview.xml');
-    Route::get('event.xml',                     [InsightSitemapController::class, 'eventsitemap'])->name('event.xml');
-    Route::get('report.xml',                    [InsightSitemapController::class, 'reportsitemap'])->name('report.xml');
-    Route::get('categories.xml',                [InsightSitemapController::class, 'categorysitemap'])->name('categories.xml');
-    Route::get('subcategories.xml',             [InsightSitemapController::class, 'subcategorysitemap'])->name('subcategories.xml');
-    Route::get('kickers.xml',                   [InsightSitemapController::class, 'kickersitemap']);
-    Route::get('tags.xml',                      [InsightSitemapController::class, 'tagsitemap'])->name('tags.xml');
+    Route::group(['prefix' => 'en'], function () {
+        Route::get('news.xml',                      [InsightSitemapController::class, 'newssitemap']);
+        Route::get('article.xml',                   [InsightSitemapController::class, 'articlesitemap'])->name('article.xml');
+        Route::get('interview.xml',                 [InsightSitemapController::class, 'interviewsitemap'])->name('interview.xml');
+        Route::get('event.xml',                     [InsightSitemapController::class, 'eventsitemap'])->name('event.xml');
+        Route::get('report.xml',                    [InsightSitemapController::class, 'reportsitemap'])->name('report.xml');
+        Route::get('categories.xml',                [InsightSitemapController::class, 'categorysitemap'])->name('categories.xml');
+        Route::get('subcategories.xml',             [InsightSitemapController::class, 'subcategorysitemap'])->name('subcategories.xml');
+        Route::get('kickers.xml',                   [InsightSitemapController::class, 'kickersitemap']);
+        Route::get('tags.xml',                      [InsightSitemapController::class, 'tagsitemap'])->name('tags.xml');
+    });
+    Route::group(['prefix' => 'hi'], function () {
+        Route::get('news.xml',                      [InsightSitemapController::class, 'newssitemap']);
+        Route::get('article.xml',                   [InsightSitemapController::class, 'hindiarticlesitemap'])->name('article.xml');
+        Route::get('interview.xml',                 [InsightSitemapController::class, 'interviewsitemap'])->name('interview.xml');
+        Route::get('event.xml',                     [InsightSitemapController::class, 'eventsitemap'])->name('event.xml');
+        Route::get('report.xml',                    [InsightSitemapController::class, 'reportsitemap'])->name('report.xml');
+        Route::get('categories.xml',                [InsightSitemapController::class, 'categorysitemap'])->name('categories.xml');
+        Route::get('subcategories.xml',             [InsightSitemapController::class, 'subcategorysitemap'])->name('subcategories.xml');
+        Route::get('kickers.xml',                   [InsightSitemapController::class, 'kickersitemap']);
+        Route::get('tags.xml',                      [InsightSitemapController::class, 'tagsitemap'])->name('tags.xml');
+    });
 });
 
 Route::middleware(['TrailingSlashRedirect'])->group(function () {
@@ -1142,4 +1158,4 @@ Route::get('/l_layout', [CommonController::class, 'listing_layout']);
 Route::post('/fetch-data', [CommonController::class, 'fetchDataajax']);
 Route::post('/price_filter', [BusinessListingController::class, 'pricefilter']);
 Route::post('/fetch-data2', [CommonController::class, 'fetchDataajax2']);
-Route::post('/price_filter', [BusinessListingController::class,'pricefilter']);
+Route::post('/price_filter', [BusinessListingController::class, 'pricefilter']);
