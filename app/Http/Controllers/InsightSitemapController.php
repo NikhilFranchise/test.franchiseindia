1b78c745b810a209b696389b5951eb1da8a7591d
GaneshFI <pganesh@franchiseindia.net>
2024-12-12 17:30:34 +0530

    dasdas

--- app/Http/Controllers/InsightSitemapController.php
+++ app/Http/Controllers/InsightSitemapController.php
@@ -31,7 +31,7 @@ class InsightSitemapController extends Controller
         $articlesitemap = InsightList::query()
         ->where('insight_type', 'Article')
             ->where('cat_id', '!=', '')
-            ->where('status', 1)->limit(5000)
+            ->where('status', 1)->limit(15000)
             ->get();
 
         //  dd($articlesitemap);
