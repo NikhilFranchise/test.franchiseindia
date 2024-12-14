105cadade8ce4c2297b8dc054c9b8c2542a76e7f
GaneshFI <pganesh@franchiseindia.net>
2024-12-14 13:54:26 +0530

    changes

--- resources/views/insights/sitemaps/subcat_sitemap.blade.php
+++ resources/views/insights/sitemaps/subcat_sitemap.blade.php
@@ -1,7 +1,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($subcategories as $sdata)
         <url>
-            <loc>{{ url('/') }}/insights/{{ strtolower($sdata['category']->slug) }}/{{ strtolower($sdata['scategory']->slug) }}</loc>
+            <loc>{{ url('/') }}/insights/en/{{ strtolower($sdata['category']->slug) }}/{{ strtolower($sdata['scategory']->slug) }}</loc>
             <lastmod>{{ date('d-m-Y h:m A', strtotime($sdata['created_at'])) }}</lastmod>
         </url>
     @endforeach
--- resources/views/insights/sitemaps/tags_sitemap.blade.php
+++ resources/views/insights/sitemaps/tags_sitemap.blade.php
@@ -1,7 +1,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($tagsData as $tag)
         <url>
-            <loc>{{ url('/') }}/insights/tag/{{ strtolower(str_replace(' ', '-', $tag['tag'])) }}</loc>
+            <loc>{{ url('/') }}/insights/en/tag/{{ strtolower(str_replace(' ', '-', $tag['tag'])) }}</loc>
             <lastmod>{{ date('d-m-Y h:m A', strtotime($tag['created_at'])) }}</lastmod>
         </url>
     @endforeach
