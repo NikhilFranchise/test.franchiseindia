48b4ab5e3b2648864943fcb3e34108e49d212e1c
GaneshFI <pganesh@franchiseindia.net>
2025-02-04 15:14:54 +0530

    changes in detail ,filesystem, menu of insights

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,17 +38,10 @@ return [
             'throw' => false,
         ],
 
-        // 'public' => [
-        //     'driver' => 'local',
-        //     'root' => storage_path('app/public'),
-        //     'url' => env('APP_URL').'/storage',
-        //     'visibility' => 'public',
-        //     'throw' => false,
-        // ],
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'url' => env('APP_URL') . '/storage/app/public',
             'visibility' => 'public',
             'throw' => false,
         ],
