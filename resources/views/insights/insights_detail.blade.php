5d4e80b7311f0d0aeddf3783df25a4b66cae1c4a
GaneshFI <pganesh@franchiseindia.net>
2025-02-07 16:50:45 +0530

    changes in temp

---
+++ resources/views/insights/insights_detail.blade.php
@@ -0,0 +1,343 @@
+@extends('layout.insights.master')
+@section('seoTitle', $newsDetails->title)
+@section('header-schema')
+    @include('insights.schema', ['newsDetails' => $newsDetails])
+@endsection
+@section('seoDesc', $newsDetails->shortDesc)
+@section('seoKeywords', $newsDetails->kicker)
+@section('canonicalUrl', url()->current())
+{{-- @dd($newsDetails->image); --}}
+@php
+    $ogimage = !empty($newsDetails->image)
+        ? \App\Http\Controllers\InsightsController::createimgurl($newsDetails->image)
+        : 'https://franchiseindia.s3.ap-south-1.amazonaws.com/uploads/content/fi/int/5ff40e6aaa3da.jpeg';
+    $imageDetails = @getimagesize($ogimage);
+    $width = $imageDetails[0] ? $imageDetails[0] : 0;
+    $height = $imageDetails[1] ? $imageDetails[1] : 0;
+    $locale = App::getLocale();
+    $baseUrl = Config('constants.MainDomain') . "/insights/$locale/";
+    $newsUrl =
+        $baseUrl . strtolower($newsDetails->insight_type) . '/' . $newsDetails->slug . '.' . $newsDetails->news_id;
+    //$author_details
+    $authorSlug = $author_details->slug ?? strtolower(str_replace(' ', '-', $author_details->title));
+    //dd($authorSlug);
+    $authorUrl = Config('constants.MainDomain') . '/insights/author/' . $authorSlug . '-' . $author_details->author_id;
+    $authorImage = !empty($author_details->image)
+        ? \App\Http\Controllers\InsightsController::authorImageurl($author_details->image)
+        : url('images/defaultuser.png');
+@endphp
+<style>
+    .article-wrapper {
+        margin-top: 40px;
+        padding-top: 40px;
+        border-top: 1px solid #eee;
+    }
+
+    #loading {
+        text-align: center;
+        padding: 20px;
+        font-size: 1.2em;
+        background: #f5f5f5;
+    }
+</style>
+@section('image', $ogimage)
+@section('shortDesc', $newsDetails->shortDesc)
+@section('imagesrc', $ogimage)
+@section('title', $newsDetails->title)
+@section('url', url()->current())
+@section('width', $width)
+@section('height', $height)
+@section('content')
+    <div class="maininnver homeh">
+        <div class="inner-top-head">
+            <div class="container">
+                <h1>{{ $newsDetails->title }}</h1>
+            </div>
+        </div>
+        <!-- DESKTOP TOP AD PLACEMENT  -->
+        <div class="container">
+            @desktop
+                <div class="inner-article-detail-desktop-top-ad">
+                    <div id='adslot728x90_ATF'>
+                        <script>
+                            googletag.cmd.push(function() {
+                                googletag.display('adslot728x90_ATF');
+                            });
+                        </script>
+                    </div>
+                </div>
+            @enddesktop
+        </div>
+        <!-- DESKTOP TOP AD PLACEMENT  -->
+    </div>
+    <div class="contentwrapper">
+        <div class="container">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="{{ url('/insights') }}" class="tip-bottom">Home</a>
+                        </li>
+                        @php
+                            $insightTypeMap = [
+                                'News' => "{$locale}/topstories",
+                                'Article' => "{$locale}/industryfocus",
+                                'Event' => "{$locale}/events_reports",
+                                'Report' => "{$locale}/events_reports",
+                                'Interview' => "{$locale}/interviews",
+                            ];
+                            $typeURL = $insightTypeMap[$newsDetails->insight_type] ?? null;
+                        @endphp
+                        <li class="breadcrumb-item">
+                            <a href="{{ url("/insights/{$typeURL}") }}"
+                                class="tip-bottom">{{ $newsDetails->insight_type }}</a>
+                        </li>
+                        @foreach ($newsDetails->category as $category)
+                            <li class="breadcrumb-item"><a href="{{ $baseUrl . $category->slug }}"
+                                    class="tip-bottom">{{ $category->catname }}</a></li>
+                        @endforeach
+                        @foreach ($newsDetails->Subcategory as $subcat)
+                            <li class="breadcrumb-item"><a
+                                    href="{{ $baseUrl . $category->slug . '/' . $subcat->slug }}">{{ $subcat->subcat_name }}</a>
+                            </li>
+                        @endforeach
+                        @desktop
+                            <li class="breadcrumb-item">{!! html_entity_decode(\Illuminate\Support\Str::words($newsDetails->title, 8, ' ...'), ENT_QUOTES, 'UTF-8') !!}</li>
+                        @enddesktop
+                    </ul>
+                    <h2>{{ $newsDetails->title }}</h2>
+                    <div class="cont-top">
+                        <div class="article-features">
+                            <div class="article-date">
+                                <div class="article-logo">
+                                    <img src="{{ $authorImage }}" width="51" height="51" alt="Indian Retailer"
+                                        loading="lazy" class="">
+                                </div>
+                                <div class="article-time">
+                                    BY -
+                                    <a href="{{ $authorUrl }}">{{ $author_details->title }}</a>
+                                    <br>
+                                    <span>
+                                        {{ $author_details->designation }}
+                                    </span>
+                                    <span>
+                                        {{ date('M d, Y', strtotime($newsDetails->created_at)) }} /
+                                        <img src="{{ url('/insight-new/images/vicon.webp') }}" height="10"
+                                            width="17" alt="Franchise Insights" class="img-fluid">
+                                        {{ $newsDetails->views }}
+                                        / {{ app\Http\Controllers\InsightsController::calculateReadTime($newsDetails) }}
+                                        Min Read
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="content-share">
+                                <ul>
+                                    <li>
+                                        <a target="_blank"
+                                            href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($newsUrl) }}">
+                                            <img src="{{ url('insight-new/images/fshare.webp') }}" height="25"
+                                                width="25" loading="lazy" alt="IR">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank"
+                                            href="http://www.linkedin.com/shareArticle?mini=true&amp;url={{ $newsUrl }}">
+                                            <img src="{{ url('insight-new/images/flink.webp') }}" height="25"
+                                                width="25" loading="lazy" alt="Insights">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" href="https://x.com/intent/post?url={{ $newsUrl }}">
+                                            <img src="{{ url('insight-new/images/ftwit.webp') }}" height="25"
+                                                width="25" loading="lazy" alt="Insights">
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="follow-us">
+                                <a href="" target="_blank">
+                                    Follow Us
+                                    <img src="{{ url('insight-new/images/follow.webp') }}" loading="lazy"
+                                        alt="Franchise India" width="11" height="10">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content-main">
+                        <img src="{{ $ogimage }}" class="img-fluid" alt="{{ $newsDetails->title }}">
+                        {{-- ads for mobile & desktop --}}
+                        <div class="inner-article-detail-desktop-ad fad">
+                            <div id="adslotInline_3_300x250">
+                                <script>
+                                    googletag.cmd.push(function() {
+                                        googletag.display("adslotInline_3_300x250");
+                                    });
+                                </script>
+                            </div>
+                        </div>
+                        {{-- ads for mobile & desktop --}}
+                        <div class="shortdes">{{ $newsDetails->shortDesc }}</div>
+                        <div class="articlecontent">
+                            <!-- Content before ads -->
+                            <div class="content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+                            </div>
+
+                            <!-- Placeholder for ads (they will be injected dynamically) -->
+                            <div id="ad-container"></div>
+
+                            <!-- Content after ads -->
+                            <div class="content">
+                                <p>More content here...</p>
+                            </div>
+
+                            <script>
+                                googletag.cmd.push(function() {
+                                    let adContainer = document.getElementById("ad-container");
+
+                                    // Loop to create 5 ad slots dynamically
+                                    for (let i = 1; i <= 5; i++) {
+                                        let adDiv = document.createElement("div");
+                                        let uniqueId = "adslotInline_" + i + "_300x250";
+                                        adDiv.className = "inner-article-detail-desktop-ad fad ad-slot";
+                                        adDiv.id = uniqueId;
+                                        adContainer.appendChild(adDiv); // Append to content
+
+                                        // Define the ad slot
+                                        googletag.defineSlot('/1057625/FIHL/FI_Desktop_ROS_Inline_1_300x250', [
+                                                [300, 250],
+                                                [336, 280],
+                                                [250, 250]
+                                            ], uniqueId)
+                                            .addService(googletag.pubads());
+
+                                        googletag.display(uniqueId); // Display the ad
+                                    }
+
+                                    googletag.enableServices(); // Enable Google Ads
+                                });
+                            </script>
+
+                        </div>
+                        @if (!empty($franchiseData))
+                            <div style="display: flex; flex-wrap: wrap; gap: 15px; justify-content: center; padding: 20px;">
+                                <h4 style="margin-top:15px">Interested in Franchise:</h4>
+                                @foreach ($franchiseData as $franchise)
+                                    <div
+                                        style="background-color: #f9f9f9; border: 1px solid #ddd; padding: 15px 20px; border-radius: 8px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); text-align: center; min-width: 95px; max-width: 200px;">
+                                        <a href="http://franchiseindia.com/brands/{{ strtolower($franchise['profile_name']) }}.{{ $franchise['fran_detail_id'] }}"
+                                            target="_blank"
+                                            style="text-decoration: none; color: #333; font-weight: bold; font-size: 16px;">
+                                            {{ $franchise['company_name'] }}
+                                        </a>
+                                    </div>
+                                @endforeach
+                            </div>
+                        @endif
+                        <div class="tag-block">
+                            <ul class="tag-list">
+                                @if (!empty($assocTags) && isset($assocTags))
+                                    @foreach ($assocTags as $assocTagsData)
+                                        @php
+                                            $tags = str_replace(' ', '-', $assocTagsData->name);
+                                            $tagslug = strtolower($tags);
+                                        @endphp
+                                        <li><a
+                                                href="{{ Config('constants.MainDomain') . '/insights/' . $locale . '/tag/' . $tagslug }}">{{ $assocTagsData->name }}</a>
+                                        </li>
+                                    @endforeach
+                                @endif
+                            </ul>
+                        </div>
+                    </div>
+                    {{-- <div class="contentarea">
+                        @include('layout.insights.subscribenewsletter')
+                    </div> --}}
+                </div>
+                <div class="col-md-4">
+                    <div class="right-wrap">
+                        {{-- ads top right sidebar --}}
+                        <div class="ad-right">
+                            <div id='adslot300x250_ATF'>
+                                <script>
+                                    googletag.cmd.push(function() {
+                                        googletag.display('adslot300x250_ATF');
+                                    });
+                                </script>
+                            </div>
+                        </div>
+                        {{-- ads top right sidebar --}}
+                        {{-- <div class="popular-articles">
+                            <h3>Related Content</h3>
+                            <ul>
+                                @forelse ($trendingArticles as $trend)
+                                    <li>
+                                        @foreach ($trend->category as $cat)
+                                            @php
+                                                $locale = App::getLocale();
+                                                $catURL =
+                                                    Config('constants.MainDomain') . "/insights/{$locale}/{$cat->slug}";
+                                                $baseUrl1 =
+                                                    Config('constants.MainDomain') .
+                                                    "/insights/$locale/" .
+                                                    strtolower($trend->insight_type) .
+                                                    '/';
+                                                $trendUrl = $baseUrl1 . $trend->slug . '.' . $trend->news_id;
+                                            @endphp
+                                          
+                                        @endforeach
+                                        <div class="popular-head">
+                                            <a href="{{ $trendUrl }}">{{ $trend->title }}</a>
+                                        </div>
+                                    </li>
+                                @empty
+                                    <li>No Records.</li>
+                                @endforelse
+                            </ul>
+                        </div>
+                        <div class="popular-articles">
+                            <h3>Latest Articles</h3>
+                            <ul>
+                                @forelse ($latestArticles as $latest)
+                                    <li>
+                                        @foreach ($latest->category as $cat)
+                                            @php
+                                                $locale = App::getLocale();
+                                                $catURL =
+                                                    Config('constants.MainDomain') . "/insights/{$locale}/{$cat->slug}";
+                                                $baseUrl1 =
+                                                    Config('constants.MainDomain') .
+                                                    "/insights/$locale/" .
+                                                    strtolower($latest->insight_type) .
+                                                    '/';
+                                                $latestUrl = $baseUrl1 . $latest->slug . '.' . $latest->news_id;
+                                            @endphp
+                                           
+                                        @endforeach
+                                        <div class="popular-head">
+                                            <a href="{{ $latestUrl }}">{{ $latest->title }}</a>
+                                        </div>
+                                    </li>
+                                @empty
+                                @endforelse
+                            </ul>
+                        </div> --}}
+                        <div class="ad-right-sticky">
+                            <div id="adslot300x250_1">
+                                <script>
+                                    googletag.cmd.push(function() {
+                                        googletag.display('adslot300x250_1');
+                                    });
+                                </script>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    {{-- @include('layout.insights.magblock') --}}
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1080,8 +1080,9 @@ Route::group(['prefix' => 'insights'], function () {
 });
 
 Route::middleware(['TrailingSlashRedirect'])->group(function () {
-
-
+    // temporally
+    Route::get('/{slug}.{id}',    [InsightsController::class, 'getInsightsDetails1']);
+    //temporally
     Route::group(['prefix' => 'insights'], function () {
         Route::get('/hindi',                        [InsightsController::class, 'insightshome'])->name('NewsHiHome');
         Route::get('/',                             [InsightsController::class, 'insightshome'])->name('newsEnHome');
@@ -1203,11 +1204,13 @@ Route::get('/getajax', [BusinessListingController::class, 'fetchtest']);
 
 Route::get('/items', [BusinessListingController::class, 'index'])->name('items.index');
 
-Route::get('advertise',                   function() { return redirect('https://www.franchiseindia.com/advertise-with-us-payment', 301);});
+Route::get('advertise',                   function () {
+    return redirect('https://www.franchiseindia.com/advertise-with-us-payment', 301);
+});
 
 
 // test email route
-Route::get('/sendmail',[CommonController::class,'send_email']);
+Route::get('/sendmail', [CommonController::class, 'send_email']);
 // Route::post('/password/email',[CommonController::class,'reset']);
 
 Route::get('/password-reset', [CommonController::class, 'thankYou'])->name('password-reset');
