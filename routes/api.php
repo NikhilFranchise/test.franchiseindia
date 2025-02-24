ae9599ddc31fc6a7c61b03647c755676651b4eac
NikhilFranchise <cnikhil@franchiseindia.net>
2025-02-24 10:54:35 +0530

    api.php merge

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use App\Http\Controllers\DealersAndDistributorController;
-
-=======
->>>>>>> 2d56f9601e98ea3920a58ef0c5e23b6875874c53
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,12 +17,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
-Route::group(['middleware' => 'cors'], function () {
-    // Your routes here
-Route::post('/investor-registration', [DealersAndDistributorController::class, 'dealerhomepage']);
-
-});
-
-=======
->>>>>>> 2d56f9601e98ea3920a58ef0c5e23b6875874c53
