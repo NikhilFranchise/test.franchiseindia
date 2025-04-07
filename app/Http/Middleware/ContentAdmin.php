c718d6a4a1a57fd1efcd9e3c05945482c88051ff
GaneshFI <pganesh@franchiseindia.net>
2025-04-07 10:26:54 +0530

    test for login

--- app/Http/Middleware/ContentAdmin.php
+++ app/Http/Middleware/ContentAdmin.php
@@ -18,7 +18,7 @@ class ContentAdmin
     {
         // if (!($request->session()->has('adminEmail')))
         //     return redirect('admin/login');
-        dd($request->session()->has('adminEmail'));
+        // dd($request->session()->has('adminEmail'));
         if (!Auth::guard('admin')->check()) {
             return redirect('admin/login')->with('error', 'You must be an admin to access this page.');
         }
