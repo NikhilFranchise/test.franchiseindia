93a15f9bb5f5883ebc9ca786367299c8767edf61
GaneshFI <pganesh@franchiseindia.net>
2025-07-22 15:13:19 +0530

    Brand Details page optimization using services and private methods injecting in brandDetails function

--- resources/views/includes/brandlanding/investment_tab.blade.php
+++ resources/views/includes/brandlanding/investment_tab.blade.php
@@ -64,6 +64,7 @@
             </div>
 
         @endif
+        @dd($franTradePartnerData);
         @if ($franTradePartnerData->count() > 0)
             <div class="section">
                 <div class="ttl">Trade partners</div>
