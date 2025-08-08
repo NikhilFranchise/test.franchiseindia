dfe5e54d816543f2c2ae6b89b429b48a3637512b
GaneshFI <pganesh@franchiseindia.net>
2025-08-08 12:44:36 +0530

    change in insta apply form in all layouts

--- app/Models/MobileVerification.php
+++ app/Models/MobileVerification.php
@@ -9,4 +9,13 @@ class MobileVerification extends Model
 {
     use HasFactory;
     public $table = 'mobile_verification';
+
+    protected $fillable = [
+        'user_id',
+        'mobile_no',
+        'otp_code',
+        'smspg_response',
+        'is_verified',
+        'verified_at'
+    ];
 }
