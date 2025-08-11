d89ab1e40b3a1ec2b79b1bb5527501b1eee1ba94
Pankajkumar0333 <pkumar@franchiseindia.net>
2025-08-11 12:33:09 +0530

    Pankaj insights

--- resources/views/layout/insights/headerlinks.blade.php
+++ resources/views/layout/insights/headerlinks.blade.php
@@ -1,23 +1,6 @@
- {{-- <!-- Favicons -->
- <link href="{{url('insight-new/assets/img/favicon.ico')}}" rel="icon">
- <link href="{{url('insight-new/assets/img/favicon-new.png')}}" rel="apple-touch-icon">
- <!-- Google Fonts -->
- <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;600;700;900&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;600;700&display=swap" rel="stylesheet">
- <!-- Vendor CSS Files -->
- <link href="{{url('insight-new/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
- <link href="{{url('insight-new/assets/vendor/icofont/icofont.min.css')}}" rel="stylesheet">
- <link href="{{url('insight-new/assets/vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
- <link href="{{url('insight-new/assets/vendor/aos/aos.css')}}" rel="stylesheet">
-<link href="{{url('insight-new/assets/css/swiper.min.css')}}" rel="stylesheet">
- <link href="{{url('insight-new/assets/css/insightstyle.css?ver='.date('d'))}}" rel="stylesheet">
- <link href="{{url('insight-new/assets/css/insight_new.css?ver='.date('d'))}}" rel="stylesheet">
-   <!-- Java Script --> --}}
-
 <!-- Favicons -->
-<link href="{{url('insight-new/assets/img/favicon.ico')}}" rel="icon">
-<link href="{{url('insight-new/assets/img/favicon-new.png')}}" rel="apple-touch-icon">
+<link href="{{ url('insight-new/assets/img/favicon.ico') }}" rel="icon">
+<link href="{{ url('insight-new/assets/img/favicon-new.png') }}" rel="apple-touch-icon">
 
 <!-- Performance hints -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -32,20 +15,20 @@
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;600;700&display=swap" rel="stylesheet">
 
 <!-- Vendor CSS Files -->
-<link href="{{url('insight-new/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-<link href="{{url('insight-new/assets/vendor/icofont/icofont.min.css')}}" rel="stylesheet" media="print" onload="this.media='all'">
-<link href="{{url('insight-new/assets/vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet" media="print" onload="this.media='all'">
-<link href="{{url('insight-new/assets/vendor/aos/aos.css')}}" rel="stylesheet" media="print" onload="this.media='all'">
-<link href="{{url('insight-new/assets/css/swiper.min.css')}}" rel="stylesheet" media="print" onload="this.media='all'">
+<link href="{{ url('insight-new/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ url('insight-new/assets/vendor/icofont/icofont.min.css') }}" rel="stylesheet" media="print" onload="this.media='all'">
+<link href="{{ url('insight-new/assets/vendor/owl.carousel/assets/owl.carousel.min.css') }}" rel="stylesheet" media="print" onload="this.media='all'">
+<link href="{{ url('insight-new/assets/vendor/aos/aos.css') }}" rel="stylesheet" media="print" onload="this.media='all'">
+<link href="{{ url('insight-new/assets/css/swiper.min.css') }}" rel="stylesheet" media="print" onload="this.media='all'">
 
 <!-- Site CSS (critical) -->
-<link href="{{url('insight-new/assets/css/insightstyle.css?ver='.date('d'))}}" rel="stylesheet">
-<link href="{{url('insight-new/assets/css/insight_new.css?ver='.date('d'))}}" rel="stylesheet">
+<link href="{{ url('insight-new/assets/css/insightstyle.css?ver='.date('d')) }}" rel="stylesheet">
+<link href="{{ url('insight-new/assets/css/insight_new.css?ver='.date('d')) }}" rel="stylesheet">
 <!-- Java Script -->
 
 @mobile
-  <script src="{{ mix('insight-mobile.js') }}" defer></script>
+  <script src="{{ url('insight-new/assets/js/insight-mobile.js') }}" defer></script>
 @endmobile
 @desktop
-  <script src="{{ mix('insight-desktop.js') }}" defer></script>
+  <script src="{{ url('insight-new/assets/js/insight-desktop.js') }}" defer></script>
 @enddesktop
