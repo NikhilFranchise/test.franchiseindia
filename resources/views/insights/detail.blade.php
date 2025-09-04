0e467672732e0929e325bc0ca2996b455cd4c3be
Pankajkumar0333 <pkumar@franchiseindia.net>
2025-09-04 21:51:54 +0530

    Oankajteast

--- resources/views/insights/detail.blade.php
+++ resources/views/insights/detail.blade.php
@@ -211,11 +211,11 @@
                             $relativePath = str_replace('https://franchiseindia.s3.ap-south-1.amazonaws.com/uploads/', '', $ogimage );
 
                          @endphp
-                         <img src="{{ url('img/1600x940/' . $relativePath) }}" alt="{{ $newsDetails->title }}">
+                         {{-- <img src="{{ url('img/1600x940/' . $relativePath) }}" alt="{{ $newsDetails->title }}"> --}}
 
-                       {{-- <picture>
-                                <source srcset="{{ url('img/1600x940/' . $relativePath) }}" media="(max-width: 768px)">
-                                <source srcset="{{ url('img/1600x940/' . $relativePath) }}"  media="(min-width: 769px)">
+                       <picture>
+                                {{-- <source srcset="{{ url('img/1600x940/' . $relativePath) }}" media="(max-width: 768px)">
+                                <source srcset="{{ url('img/1600x940/' . $relativePath) }}"  media="(min-width: 769px)"> --}}
                                 <img src="{{ url('img/1600x940/' . $relativePath) }}" alt="{{ $newsDetails->title }}" class="img-fluid"
                                     loading="eager"
                                     decoding="async"
@@ -223,7 +223,7 @@
                                     style="aspect-ratio: 1600 / 940;"
                                 >
                             </picture>
-                          --}}
+                         
 
                         {{-- <img src="{{ $ogimage }}" class="img-fluid" alt="{{ $newsDetails->title }}"> --}}
                         {{-- ads for mobile & desktop --}}
